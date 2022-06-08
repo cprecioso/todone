b78cea0004de8e802efb50449e63828c1d27942e
Carlos Precioso <github@precioso.design>
2022-06-08 19:23:34 +0200

    Use default-plugins

--- packages/todone/src/index.ts
+++ packages/todone/src/index.ts
@@ -1,19 +1,15 @@
 import runTodone from "@todone/core";
-import CaniusePlugin from "@todone/plugin-caniuse";
-import DatePlugin from "@todone/plugin-date";
-import FigmaCommentPlugin from "@todone/plugin-figma";
-import GitHubIssuePlugin from "@todone/plugin-github";
+import { allPlugins } from "@todone/default-plugins";
 import reporter from "@todone/reporter-cli";
 import type { File } from "@todone/types";
 import { src } from "vinyl-fs";
 
 const todone = async (globs: string[]) => {
-  await runTodone(() => src(globs) as AsyncIterable<File>, reporter, [
-    DatePlugin(),
-    CaniusePlugin(),
-    GitHubIssuePlugin(),
-    FigmaCommentPlugin(),
-  ]);
+  await runTodone(
+    () => src(globs) as AsyncIterable<File>,
+    reporter,
+    allPlugins.map((pluginFn) => pluginFn())
+  );
 };
 
 export default todone;
