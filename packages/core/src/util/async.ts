5d6482446aff7c4c66e6247f144c78d8040cf123
Carlos Precioso <github@precioso.design>
2022-08-07 22:49:00 +0200

    Remove unused function

--- packages/core/src/util/async.ts
+++ packages/core/src/util/async.ts
@@ -3,13 +3,3 @@ export const collectAsyncIterable = async <T>(it: AsyncIterable<T>) => {
   for await (const el of it) items.push(el);
   return items;
 };
-
-export const asyncMap = async <T, U>(
-  it: Iterable<T>,
-  fn: (el: T) => Promise<U>
-) =>
-  await collectAsyncIterable(
-    (async function* () {
-      for (const el of it) yield fn(el);
-    })()
-  );
