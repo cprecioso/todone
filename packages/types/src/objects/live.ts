be0c2c566c506c5974c84735751f7064d8529306
Carlos Precioso <github@precioso.design>
2025-05-16 22:29:41 +0200

    Move to regular Readables

--- packages/types/src/objects/live.ts
+++ packages/types/src/objects/live.ts
@@ -1,9 +1,8 @@
-/// <reference types="node" />
 import type { BaseMatch, BaseResult } from "./base";
 
 export interface File {
   url: URL;
-  getContent: () => Promise<NodeJS.ReadableStream>;
+  getContent: () => Promise<ReadableStream>;
   isPresent: boolean;
 }
 export interface Match extends BaseMatch<File> {}
