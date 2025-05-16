d9a62c2854969caccea073d8e675545822b02677
Carlos Precioso <github@precioso.design>
2025-05-16 22:19:52 +0200

    Add x to strict mode

--- scripts/check-all.sh
+++ scripts/check-all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # "Strict" mode (https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/)
-set -euo pipefail
+set -euxo pipefail
 
 TSC=$(yarn bin tsc)
 WORKSPACES=$(yarn workspaces list --json | jq -r '.location')
