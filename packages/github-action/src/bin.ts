2438de7da3066b15f4563cff472dd3c149d1a43e
Carlos Precioso <github@precioso.design>
2025-08-06 16:42:10 +0200

    feat: add keyword option

--- packages/github-action/src/bin.ts
+++ packages/github-action/src/bin.ts
@@ -14,13 +14,14 @@ import { makeSummary } from "./lib/summary";
 const githubToken = core.getInput("github-token", { required: true });
 const globs = core.getInput("globs", { required: true });
 const createIssues = core.getBooleanInput("create-issues");
+const keyword = core.getInput("keyword", { required: true });
 
 const warnLogger = (line: string): void => core.warning(line);
 
 const plugins = await makePlugins(githubToken);
 const files = makeFileStream(globs);
 
-const reportStream = analyzeStream(files, { plugins, warnLogger })
+const reportStream = analyzeStream(files, { plugins, warnLogger, keyword })
   .pipeThrough(s.tap(makeDebugLogger()))
   .pipeThrough(s.tap(makeResultLogger()));
 
