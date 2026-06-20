e27f9208c68680b98ca8ffeb7d8074ffdcf0f271
Carlos Precioso <github@precioso.design>
2026-06-20 11:44:17 +0200

    chore: upgrade type-fest

--- packages/github-action/src/lib/plugins.ts
+++ packages/github-action/src/lib/plugins.ts
@@ -2,14 +2,13 @@ import * as NodeContext from "@effect/platform-node/NodeContext";
 import * as HttpClient from "@effect/platform/HttpClient";
 import { Plugin, PluginFactory } from "@todone/types";
 import * as Effect from "effect/Effect";
+import type { NonEmptyTuple } from "type-fest";
 
 export type AllowedPlugin = PluginFactory<
   NodeContext.NodeContext | HttpClient.HttpClient
 >;
 
-type NonEmpty<T> = readonly [T, ...(readonly T[])];
+export type AllowedPluginList = NonEmptyTuple<AllowedPlugin>;
 
-export type AllowedPluginList = NonEmpty<AllowedPlugin>;
-
-export const makePlugins = (plugins: NonEmpty<AllowedPlugin>) =>
+export const makePlugins = (plugins: AllowedPluginList) =>
   Effect.all(plugins.map((plugin) => Effect.provide(Plugin, plugin)));
