922beff11200d561c8fbdca262a06ab0ad4f978d
Carlos Precioso <github@precioso.design>
2026-06-20 22:33:12 +0200

    feat: add more information to the JSON schema

--- packages/todone/src/lib/config/schema.ts
+++ packages/todone/src/lib/config/schema.ts
@@ -2,27 +2,48 @@ import * as Schema from "effect/Schema";
 
 export const BUILTIN_PLUGIN_NAME = "builtin";
 
+const optionalKey = <S extends Schema.Schema.All>(
+  schema: S,
+  defaultValue: Schema.Schema.Type<S>,
+) =>
+  Schema.optionalWith(schema, { default: () => defaultValue }).annotations({
+    default: defaultValue,
+  });
+
 export const Config = Schema.Struct({
-  keyword: Schema.optionalWith(Schema.String, { default: () => "@TODO" }),
+  $schema: Schema.optional(Schema.String),
 
-  gitignore: Schema.optionalWith(Schema.Boolean, { default: () => true }),
-
-  globs: Schema.optionalWith(Schema.Array(Schema.String), {
-    default: () => ["**/*"],
+  keyword: optionalKey(Schema.String, "@TODO").annotations({
+    description: "The keyword to use for TODO comments.",
   }),
 
-  reporter: Schema.optionalWith(Schema.String, { default: () => "auto" }),
+  gitignore: optionalKey(Schema.Boolean, true).annotations({
+    description: "Whether to respect .gitignore files.",
+  }),
 
-  plugins: Schema.optionalWith(
+  globs: optionalKey(Schema.Array(Schema.String), ["**/*"]).annotations({
+    description: "The file patterns to include.",
+  }),
+
+  reporter: optionalKey(Schema.String, "auto").annotations({
+    description:
+      "The reporter to use. Can be 'auto', 'cli', 'json', or the ID of a custom reporter.",
+    examples: ["auto", "cli", "json", "my-custom-reporter"],
+  }),
+
+  plugins: optionalKey(
     Schema.Record({
       key: Schema.String,
-      value: Schema.Record({
-        key: Schema.String,
-        value: Schema.Unknown,
+      value: Schema.Unknown.annotations({
+        description:
+          "The configuration to pass to the plugin. Can be any valid JSON value, the plugin is in charge of validating it.",
       }),
     }),
-    { default: () => ({}) },
-  ),
+    {},
+  ).annotations({
+    description:
+      "An object with keys being the package names of plugins to load, and values being any configuration you want to pass them.",
+  }),
 });
 
 export interface Config extends Schema.Schema.Type<typeof Config> {}
